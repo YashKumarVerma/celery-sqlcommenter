587a3d364bdf333d8e58481264c439c1d905222d
Yash Verma <yk.verma2000@gmail.com>
2025-08-29 13:59:07 +0530

    feat: load project identifier from django conf

--- celery_sqlcommenter/query_wrapper.py
+++ celery_sqlcommenter/query_wrapper.py
@@ -1,11 +1,13 @@
-from .utils import add_sql_comment
+import django
 from prometheus_client import Counter
 
+from .utils import add_sql_comment
 
 CELERY_SQL_COMMENTER_METRIC = Counter(
     name="celery_sql_commenter_metric",
     documentation="queries initiated via celery tasks",
     labelnames=[
+        "project",
         "task_name"
     ]
 )
@@ -16,8 +18,10 @@ class QueryWrapper:
         self.task_name = task_name
 
     def __call__(self, execute, sql, params, many, context):
+        project = getattr(django.conf.settings, 'SQLCOMMENTER_PROJECT_ID', 'untagged')
+
         # Initialize a dictionary to hold additional comment parameters
         additional_comments = {"celery_task": self.task_name}
-        CELERY_SQL_COMMENTER_METRIC.labels(task_name=self.task_name).inc()
+        CELERY_SQL_COMMENTER_METRIC.labels(task_name=self.task_name, project=project).inc()
         sql = add_sql_comment(sql, **additional_comments)
         return execute(sql, params, many, context)
